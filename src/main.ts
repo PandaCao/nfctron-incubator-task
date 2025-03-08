6866d16179c52732b6321da9efd8f6fe9f9577ba
Duc Anh Cao <lowakcz@gmail.com>
2025-03-08 22:29:04 +0100

    chore: add explicit Typescript data types to main.js

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap(): Promise<void> {
-  const app: INestApplication = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+    const app: INestApplication = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe());
+
+    const config: Omit<OpenAPIObject, 'paths'> = new DocumentBuilder()
+        .setTitle('Cats example')
+        .setDescription('The cats API description')
+        .setVersion('1.0')
+        .addTag('cats')
+        .build();
+    const documentFactory: () => OpenAPIObject = (): OpenAPIObject =>
+        SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, documentFactory);
+
+    await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
